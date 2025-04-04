88358ae3c92d76fc1b4f6db90809492cf57514d7
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-04-04 10:05:48 +0200

    nothing is working, bad architecture, did the exo on fork. Hated it

--- old/contexts/MovieContext.tsx
+++
@@ -1,7 +0,0 @@
-
-import { createContext } from "react";
-import { MovieListOrNull, MovieListSetter } from "../App";
-
-const MovieContext = createContext<{ movies: MovieListOrNull, setMovies: MovieListSetter } | null>(null);
-
-export default MovieContext;
